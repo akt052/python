bad6b59a2bf9e2d144822d7e194c922da6751625
primary <akshat0akt52@gmail.com>
2025-04-16 09:13:19 +0530

    calculating cost

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+cost = 0
+with open('./Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    for line in f:
+        row = line.split(',')
+        numberOfShares = int(row[1])
+        priceOfShares = float(row[2])
+        cost += numberOfShares * priceOfShares
+print(cost)
